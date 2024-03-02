2016d491c429d2d84a017987d5c784ca82027dc3
zengjianhao <zengjianhao2017@gmail.com>
2024-03-03 02:39:29 +0800

    push

---
+++ scripts/test-dresscode.sh
@@ -0,0 +1,4 @@
+python -u test.py \
+--base configs/test-dresscode.yaml \
+--ckpt /home/sd/Harddisk/zjh/Teacher/epoch=80.ckpt \
+--ddim 16
---
+++ scripts/train-dresscode.sh
@@ -0,0 +1,4 @@
+python -u train.py \
+--logdir logs \
+--pretrained_model checkpoints/pbe-dim5.ckpt \
+--base configs/train-dresscode.yaml
